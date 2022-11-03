e91818e7322823d528810d38bb6e5d823627b096
Senay Gebrat <senaygebrat@gmail.com>
2022-11-03 11:24:20 -0700

    updated models

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Location = require('./Location');
+const Traveler = require('./Traveler');
+const Trip = require('./Trip');
+
+
+
+
+
+module.exports = {
+  Location,
+  Traveler,
+  Trip,
+};
